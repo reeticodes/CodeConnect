edcd87c1fdd1c44611476311bf562a69191ab540
Reeti Sharma <reetisharma105@gmail.com>
2020-12-08 17:02:48 +0530

    store

--- client/src/store.js
+++ client/src/store.js
@@ -1,28 +1,16 @@
-import { createStore, applyMiddleware ,compose, combineReducers} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-function saveToLocalStorage(state) {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
-}
-function loadFromLocalStorage() {
-const serializedState = localStorage.getItem('state');
-if (serializedState === null) return undefined;
-   return JSON.parse(serializedState);
-}
-
-
+const initialState = {};
 
 const middleware = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const presistedState = loadFromLocalStorage();
+
 export const store = createStore(
   rootReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
-store.subscribe(() => saveToLocalStorage(store.getState()));
 export default store;
