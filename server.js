a61348358a29fd95bda6818918adbbcd6e6c02c5
Reeti Sharma <reetisharma105@gmail.com>
2020-12-08 17:00:26 +0530

    revert

--- server.js
+++ server.js
@@ -16,14 +16,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/announcements', require('./routes/api/announcements'))
-if(process.env.NODE_ENV==='production')
-{
-  app.use(express.static('client/build'));
 
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'));
-  })
-}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
