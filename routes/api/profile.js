6eb3990c38caed69601cb2e84668b3e2717a768b
Reeti Sharma <reetisharma105@gmail.com>
2020-12-09 21:56:54 +0530

    repo fix

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -331,16 +331,18 @@ router.delete('/education/:exp_id', auth, async (req, res) => {
 
 router.get('/github/:username', async (req, res) => {
   try {
-    const uri = encodeURI(
-      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-    );
-    const headers = {
-      'user-agent': 'node.js',
-      Authorization: `token ${config.get('githubToken')}`
-    };
+    // const uri = encodeURI(
+    //   `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    // );
+    // const headers = {
+    //   'user-agent': 'node.js',
+    //   Authorization: `token ${config.get('githubToken')}`
+    // };
 
-    const gitHubResponse = await axios.get(uri, { headers });
-    return res.json(gitHubResponse.data);
+    // const gitHubResponse = await axios.get(uri, { headers });
+    // return res.json(gitHubResponse.data);
+    const rest = await axios.get(`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`);
+    return res.json(rest.data);
   } catch (err) {
     console.error(err.message);
     return res.status(404).json({ msg: 'No Github profile found' });
